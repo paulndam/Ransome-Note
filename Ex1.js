67130466fbfae9d9de4de2cec580d1b70dbb5aae
Paul Ndam <paulndam@Pauls-MacBook-Air.local>
2021-01-12 08:40:26 -0500

    one

---
+++ Ex1.js
@@ -0,0 +1,33 @@
+/** @format */
+
+// Ransome Note
+
+// function can spell
+// given an array of words, write a function that determines if words in that
+// array could make up a word or match a corresponding given word.
+
+function canSpell(magazine, word) {
+    const dictionary = {};
+  
+    magazine.forEach((element) => {
+      if (dictionary[element] === undefined) {
+        dictionary[element] = 0;
+      }
+      dictionary[element]++;
+    });
+  
+    for (let characters in word) {
+      const letters = word[characters];
+  
+      if (dictionary[letters] === undefined || dictionary[letters] <= 0) {
+        return false;
+      }
+      dictionary[letters]--;
+    }
+    return true;
+  }
+  
+  console.log(canSpell(["a", "b", "c", "d", "e", "f", "g", "h"], "bed"));
+  console.log(canSpell(["a", "b", "c", "d", "e", "f", "g", "h"], "cat"));
+  console.log(canSpell(["a", "b", "c", "d", "e", "f", "g", "h"], "had"));
+  
